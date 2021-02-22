f76950d3a3e70eefbcdc63c5173706f122725f80
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-02-22 11:52:01 +0100

    Initial Commit

---
+++ Primes/PrimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimesApp.swift
+//  Primes
+//
+//  Created by Łukasz Andrzejewski on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct PrimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
